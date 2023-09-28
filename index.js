d85927975446aa5ad1e5c21d670e7a1a071fb00e
SopanMore11 <140936092+SopanMore11@users.noreply.github.com>
2023-09-28 18:44:13 +0000

    added index.js to compute basic operations.

---
+++ index.js
@@ -0,0 +1,5 @@
+let a = parseInt(process.argv[2]);
+let b = parseInt(process.argv[3]);
+let result = a+b
+console.log(`The sum of two numbers ${a} and ${b} is ${result}`);
+// console.log(process.argv)
